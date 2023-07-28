e3e2a937c96a2a12a484d4db5def1d08cc5ab1a3
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-07-28 12:15:35 +0900

    feat: product conponent modification

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -2,23 +2,87 @@ import { useFeaturedProductsQuery } from "@lib/hooks/use-layout-data"
 import UnderlineLink from "@modules/common/components/underline-link"
 import ProductPreview from "@modules/products/components/product-preview"
 import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
+import Link from "next/link"
 
 const FeaturedProducts = () => {
   const { data } = useFeaturedProductsQuery()
 
   return (
-    <div className="py-12">
-      <div className="content-container py-12">
-        <div className="flex flex-col items-center text-center mb-16">
-          <span className="text-base-regular text-gray-600 mb-6">
-            Latest products
-          </span>
-          <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
-            Our freshest coffee beans are here to help you taste the best flavors.
-          </p>
-          <UnderlineLink href="/store">Explore products</UnderlineLink>
+    <div className="pt-12 pb-6">
+      <div className="py-10">
+        <div className="flex flex-wrap items-center">
+          <div className="flex justify-start text-xl font-Suite text-black-600 mb-6 font-semibold leading-normal px-8">
+            재구매율이 많은 원두
+          </div>
+
+          {/* <UnderlineLink href="/store">Explore products</UnderlineLink> */}
         </div>
-        <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8">
+        <div>
+          <div className="flex h-[56px] items-center px-8 py-8">
+            <img
+              src="/home_roast1.png"
+              alt=""
+              className="flex justify-center h-[56px] w-[56px] bg-slate-300 rounded-full"
+            />
+
+            <div className="px-4 flex-col items-center ">
+              <p className="text-base">[비브레이브] 빵튀케냐AA</p>
+              <p className="text-gray-600 font-['Noto Sans'] text-xs font-normal">
+                #애플 #블루베리 #산미
+              </p>
+            </div>
+            <div className="ml-auto flex">
+              <Link href={"/coffee"}>
+                <img src="/arrow_right.svg" alt="" />
+              </Link>
+            </div>
+          </div>
+          <div className="flex justify-end py-6">
+            <img src="/line.png" alt="line" />
+          </div>
+          <div className="flex h-[56px] items-center px-8 py-8">
+            <img
+              src="/home_roast2.png"
+              alt=""
+              className="flex justify-center h-[56px] w-[56px] bg-slate-300 rounded-full"
+            />
+
+            <div className="px-4 flex-col items-center ">
+              <p className="text-base">[비브레이브] 빵튀케냐AA</p>
+              <p className="text-gray-600 font-['Noto Sans'] text-xs font-normal">
+                #애플 #블루베리 #산미
+              </p>
+            </div>
+            <div className="ml-auto flex">
+              <Link href={"/coffee"}>
+                <img src="/arrow_right.svg" alt="" />
+              </Link>
+            </div>
+          </div>
+          <div className="flex justify-end py-6">
+            <img src="/line.png" alt="line" />
+          </div>
+          <div className="flex h-[56px] items-center px-8">
+            <img
+              src="/home_roast1.png"
+              alt=""
+              className="flex justify-center h-[56px] w-[56px] bg-slate-300 rounded-full"
+            />
+
+            <div className="px-4 flex-col items-center">
+              <p className="text-base">[비브레이브] 빵튀케냐AA</p>
+              <p className="text-gray-600 font-['Noto Sans'] text-xs font-normal">
+                #애플 #블루베리 #산미
+              </p>
+            </div>
+            <div className="ml-auto flex">
+              <Link href={"/coffee"}>
+                <img src="/arrow_right.svg" alt="" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8">
           {data
             ? data.map((product) => (
                 <li key={product.id}>
@@ -30,7 +94,7 @@ const FeaturedProducts = () => {
                   <SkeletonProductPreview />
                 </li>
               ))}
-        </ul>
+        </ul> */}
       </div>
     </div>
   )
